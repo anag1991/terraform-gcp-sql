aa06f4b73f15f27ca6c9179fd966fd283c52470c
anaghirghilijiu <ana.ghirghilijiu.com>
2022-03-31 13:20:02 +0000

    changes

--- module/sql.tf
+++ module/sql.tf
@@ -1,6 +1,6 @@
 module "sql" {
   source              = "../"
-  name                = "db"
+  name                = "databasesql"
   database_version    = "MYSQL_5_7"
   region              = "us-central1"
   zone                = "us-central1-c"
@@ -10,11 +10,12 @@ module "sql" {
   disk_size           = 10
   disk_type           = "PD_SSD"
   db_name             = "wordpress"
-  user_name           = "admin"
+  user_name           = "dbadmin"
   user_host           = "admin.com"
   user_password       = "changeme"
   deletion_protection = false
-  allowed_hosts       = ["34.132.161.247"]
+  allowed_hosts       = ["35.202.8.248"]
+  root_password       = "ctea6hnvtmtm0GKr"
 }
 
 
